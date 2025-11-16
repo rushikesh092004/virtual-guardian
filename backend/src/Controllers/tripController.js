789399532ed13004ffa4fa59dbacd9c2315d9b25
AnujNewase <newaseanuj28@gmail.com>
2025-11-16 16:49:12 +0530

    Added backend server setup + trip routes + controller placeholders

---
+++ backend/src/Controllers/tripController.js
@@ -0,0 +1,7 @@
+exports.createTrip = async (req, res) => {
+  return res.json({ message: "Trip created (placeholder)" });
+};
+
+exports.getTrips = async (req, res) => {
+  return res.json({ message: "Trips fetched (placeholder)" });
+};
